e9585282ebb6c92709b2d82d267bb4c6dd4623be
jagadish594 <jagadish594@gmail.com>
2020-05-07 13:00:01 -0700

    fixed design issues in select meal option

--- src/components/FormSelectMeal.js
+++ src/components/FormSelectMeal.js
@@ -1,32 +1,45 @@
 import React from 'react';
-import {Form, Label, Col, Row, FormText} from 'reactstrap';
+import {Label, Col, Row, FormText, Container} from 'reactstrap';
 import SelectMeal from './SelectMeal';
 import { v4 as uuidv4 } from 'uuid';
 
 const FormSelectMeal = (props) => {
+    let dotwCount = 0;
     const menuIds = [...Array(22).keys()].slice(1, 22);
+    const daysOfTheWeek = ["", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     return (
-      <div>
+      <Container>
+        <Row>
+            <Col md={3}><Label for="DaysOfTheWeek">Days Of the week</Label></Col>
+            <Col md={3} ><Label for="Breakfast">Breakfast</Label></Col>
+            <Col md={3} ><Label for="Lunch">Lunch</Label></Col>
+            <Col md={3} ><Label for="Dinner">Dinner</Label></Col>
+        </Row>
+x
         {props.meals &&
           menuIds.map((id) => {
             if (id % 3 === 1) {
+                dotwCount += 1
               return (
                 <Row form key={uuidv4()}>
-                    <Col md={2} >
+                    <Col md={3}>
+                        <Label for={daysOfTheWeek[dotwCount]}>{daysOfTheWeek[dotwCount]}</Label>
+                    </Col>
+                    <Col md={3} >
                         <SelectMeal
                             meals={props.meals}
                             onMealSelection={props.onMealSelection}
                             menuId={id}
                         />
                     </Col>
-                    <Col md={2}>
+                    <Col md={3}>
                         <SelectMeal
                             meals={props.meals}
                             onMealSelection={props.onMealSelection}
                             menuId={id += 1}
                         />
                     </Col>
-                    <Col md={2}>
+                    <Col md={3}>
                         <SelectMeal
                             meals={props.meals}
                             onMealSelection={props.onMealSelection}
@@ -35,11 +48,14 @@ const FormSelectMeal = (props) => {
                     </Col>
                 </Row>
               );
+              
             }
-
+            
             return null;
           })}
-      </div>
+          
+          
+      </Container>
     );
 }
 
